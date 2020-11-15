e12d10617472ca50da5685c078b4c412c0aff300
Albert-Ros <62722547+Albert-Ros@users.noreply.github.com>
2020-11-15 12:22:39 +0300

    Update IZ_2.pl

--- IZ_2.pl
+++ IZ_2.pl
@@ -1,12 +1,8 @@
-domains.
-int=integer.
-list=int*
-Predicates.
-delete_wr(_,[],[]).
-delete_wr(X,[X|L],L1):- delete_wr(X,L,L1).
-delete_wr(X,[Y|L],[Y|L1]):- X<>Y,delete_wr(X,L,L1).
-list_set([],[]).
-list_set([H|T],[H|T1]) :- delete_wr(H,T,T2),list_set(T2,T1).
-merge_lists([],[]).
-merge_lists(L1,L2):-append(L1,L2,List),
-                      list_set(List,X),write(X).
+repeated_element(List, Value):-
+nth0(I, List, Value),
+nth0(J, List, Value),
+I \= J.
+
+filter_unique(A, B, Result):-
+append(A, B, AB),
+exclude(repeated_element(AB), AB, Result).
